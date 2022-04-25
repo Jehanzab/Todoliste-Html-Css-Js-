5fe9fc0fc34488272d69ace709f9b4a59feb9e58
Jehanzab <jehanzabamjad@gmail.com>
2022-04-25 23:06:29 +0200

    Update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+
+
 function NewTodo(){
     var listItem = document.createElement("li");
     var inputField = document.getElementById("myInput").value;
@@ -5,11 +37,23 @@ function NewTodo(){
 
 
     listItem.appendChild(tNode);
+    if (inputField='') {
+        alert("You must write something !")
+    } else {
+        document.getElementById("myUL").appendChild(listItem);
+    }
+    document.getElementById("myInput").value = "";
 
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
 
-    document.getElementById("myUL").appendChild(listItem);
-
-
-
-
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
